788cce0c5483e9e4b31c66f508c6fb90e6254389
fidelp27 <fidel2702@gmail.com>
2023-02-19 10:22:34 -0300

    create user collection, signout updateuser isOnline state

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import { auth } from '../firebase';
+import React, { useEffect, useState } from 'react';
+import { auth, db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { doc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore';
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
-  const googleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    await signInWithPopup(auth, provider);
-    navigate('/chatbox', { replace: true });
+  const createUser = async (user) => {
+    //! creación de usuario si no existe, modifica el anterior si ya existe.
+    //? Mantiene actualizada la bbdd y evitamos chequear por nuestra cuenta la bbdd para chequear si el usuario existe
+    await setDoc(doc(db, 'users', user.uid), {
+      name: user.displayName,
+      avatar: user.photoURL,
+      createdAt: serverTimestamp(),
+      uid: user.uid,
+      friends: [],
+      friendsRequestSend: [],
+      friendsRequestReceived: [],
+      email: user.email,
+      isOnline: true,
+    });
   };
 
-  const signOut = () => {
+  const googleSignIn = async () => {
+    try {
+      //**  nueva instancia del proveedor de autenticación de Google
+      const provider = new GoogleAuthProvider();
+      //** ventana emergente para autorizar
+      await signInWithPopup(auth, provider);
+      navigate('/chatbox', { replace: true });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    //!referencia al user
+    const userUpdate = doc(db, 'users', user.uid);
+    //?modificar el user
+    await updateDoc(userUpdate, {
+      isOnline: false,
+    });
     auth.signOut();
   };
+
+  useEffect(() => {
+    if (user) {
+      createUser(user);
+    }
+  }, [user]);
 
   return (
     <nav className="nav-container">
@@ -27,7 +62,7 @@ const Navbar = () => {
             Login
           </button>
         ) : (
-          <button id="register-btn" onClick={() => signOut()}>
+          <button id="register-btn" onClick={() => signOut(user)}>
             Salir
           </button>
         )}
